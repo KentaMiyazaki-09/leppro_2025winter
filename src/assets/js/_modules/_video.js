b4c55636491ddf9c7e0c6a5d1fed9c7fba33a1d4
KentaMiyazaki-09 <k.miyazaki@rhino-inc.jp>
2025-09-29 12:59:39 +0900

    細かい修正

--- src/assets/js/_modules/_video.js
+++ src/assets/js/_modules/_video.js
@@ -1,20 +1,23 @@
 export default () => {
-  const video = document.querySelector(".video__item");
-  const videoSoundonOnButton = document.querySelector(".video-soundon-button");
-  const videoSoundOffButton = document.querySelector(".video-soundoff-button");
+  const videoes = document.querySelectorAll(".video");
 
-  if (video) {
-    videoSoundonOnButton.addEventListener("click", (el) => {
-      console.log();
-      el.currentTarget.classList.add("is-active");
-      videoSoundOffButton.classList.remove("is-active");
-      video.muted = false;
-    });
+  videoes.forEach((video) => {
+    if (video) {
+      const movieEl = video.querySelector(".video__item");
+      const onButton = video.querySelector(".video-soundon-button");
+      const offButton = video.querySelector(".video-soundoff-button");
 
-    videoSoundOffButton.addEventListener("click", (el) => {
-      el.currentTarget.classList.add("is-active");
-      videoSoundonOnButton.classList.remove("is-active");
-      video.muted = true;
-    });
-  }
+      onButton.addEventListener("click", (el) => {
+        el.currentTarget.classList.add("is-active");
+        offButton.classList.remove("is-active");
+        movieEl.muted = false;
+      });
+
+      offButton.addEventListener("click", (el) => {
+        el.currentTarget.classList.add("is-active");
+        onButton.classList.remove("is-active");
+        movieEl.muted = true;
+      });
+    }
+  });
 };
